bf5e8defca9ee96e79db07df0308546215a74947
bszeidl <szeidl.balazs@hotmail.com>
2022-12-16 20:31:06 +0100

    10 rectangles ready with for loop.

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function pageLoaded () {
     var canvas = document.getElementById('myCanvas');
     var context = canvas.getContext('2d');
-    context.fillRect(175,100,100,100);  
+
+    let size = canvas.width * .1;
+    let position = 20; 
+    
+
+    for (let i = 0; i < 10; i++) {
+        context.fillStyle = 'rgba(255,165,0,.5)';
+        context.fillRect(position,position,size,size);
+        position += size *.5;    
+    }
+     
 
 }
 
